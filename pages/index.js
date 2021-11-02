07d0a944de6a237f12db08236d8dd5d9ec03804b
rabin-nyaundi <nyaundirain@gmail.com>
2021-11-02 14:35:18 +0300

    change name

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ function Home() {
                         <div className="h-100 my-auto z-50 p-4">
                             <button className="bg-pink-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Call to action</button>
                             <p className="align-middle m-auto">
-                                Hello my name is .....</p>
+                                Hello my name is Rabin Nyaundi</p>
                         </div>
                         <div className="z-0">
                             <Image src="/Images/computer.jpeg"
